de741d5697b31552f2f227a409cad6a0bfafd829
Seonggu Kim <seonggu.de.kim@gmail.com>
2021-11-05 12:24:46 +0900

    temp commit

--- release/scripts/startup/abler/__init__.py
+++ release/scripts/startup/abler/__init__.py
@@ -50,6 +50,7 @@ from . import render_control
 from . import pref
 from . import operators
 from .lib.tracker import tracker
+from .converter import skp_converter
 
 
 # =========================================================================
