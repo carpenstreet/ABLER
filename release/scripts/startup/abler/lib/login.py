f870d29850766686a2bf60c6fd7b201ddcac641a
Youdiie <youjin@acon3d.com>
2021-12-24 13:32:47 +0900

    check ABLER first open in Darwin

--- release/scripts/startup/abler/lib/login.py
+++ release/scripts/startup/abler/lib/login.py
@@ -1,24 +1,38 @@
-import re
+import re, os
 import subprocess
+import platform
 
 
 def is_first_open():
-    # 영어일때는 `ignore` 인자 없어도 됨
-    tasks = (
-        subprocess.check_output(["tasklist"]).decode("cp949", "ignore").split("\r\n")
-    )
-    p = []
-    for task in tasks:
-        m = re.match("(.+?) +(\d+) (.+?) +(\d+) +(\d+.* K).*", task)
-        if m is not None:
-            p.append(m.group(1))
-    cnt = 0
+    if platform.system() == "Windows":
+        tasks = (
+            subprocess.check_output(["tasklist"])
+            .decode("cp949", "ignore")
+            .split("\r\n")
+        )
+        p = []
+        for task in tasks:
+            m = re.match("(.+?) +(\d+) (.+?) +(\d+) +(\d+.* K).*", task)
+            if m is not None:
+                p.append(m.group(1))
+        cnt = 0
+        for i in p:
+            if i.startswith("blender") or i.startswith("ABLER"):
+                cnt += 1
+        if cnt > 1:
+            return False
+        else:
+            return True
 
-    for i in p:
-        if i.startswith("blender") or i.startswith("ABLER"):
-            cnt += 1
+    elif platform.system() == "Darwin":
+        tmp = os.popen("ps -Af").read()
+        proccount = tmp.count("ABLER")
 
-    if cnt > 1:
-        return False
-    else:
-        return True
+        if proccount > 2:
+            return False
+        else:
+            return True
+
+    elif platform.system() == "Linux":
+        print("Linux")
+        return None
