8d6182cde8c83ec3a4ef2bee50bb366c0ac71cac
Youdiie <youjin@acon3d.com>
2021-12-24 18:09:11 +0900

    using psutil

--- release/scripts/startup/abler/lib/login.py
+++ release/scripts/startup/abler/lib/login.py
@@ -1,30 +1,12 @@
 import re, os
 import subprocess
 import platform
+import psutil
 
 
 def is_first_open():
-    if platform.system() == "Windows":
-        tasks = (
-            subprocess.check_output(["tasklist"])
-            .decode("cp949", "ignore")
-            .split("\r\n")
-        )
-        p = []
-        for task in tasks:
-            m = re.match("(.+?) +(\d+) (.+?) +(\d+) +(\d+.* K).*", task)
-            if m is not None:
-                p.append(m.group(1))
-
-        process_count = sum(
-            bool(i.startswith("blender") or i.startswith("ABLER") for i in p)
-        )
-        return process_count <= 1
-
-    elif platform.system() == "Darwin":
-        process_count = os.popen("ps -Af").read().count("ABLER")
-        return process_count <= 2
-
-    elif platform.system() == "Linux":
-        print("Linux")
-        return None
+    process_count = sum(
+        i.startswith("ABLER") or i.startswith("blender")
+        for i in (p.name() for p in psutil.process_iter())
+    )
+    return process_count <= 1
