3448cbb931426e0cb90c92841b03105e25013466
Youdiie <youjin@acon3d.com>
2021-12-28 11:12:05 +0900

    add "abler"

--- release/scripts/startup/abler/lib/login.py
+++ release/scripts/startup/abler/lib/login.py
@@ -3,8 +3,9 @@ import psutil
 
 def is_first_open():
     # 윈도우에선 blender.exe로 인식
+    # 추후 abler.exe로 만들 경우를 대비
     process_count = sum(
-        i.startswith("ABLER") or i.startswith("blender")
+        i.startswith("ABLER") or i.startswith("blender") or i.startswith("abler")
         for i in (p.name() for p in psutil.process_iter())
     )
     return process_count <= 1
